2f503117be7adf1973c588be268dc726c91b2742
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-08-04 18:28:02 +0100

    Fetching data direct from json gql server

--- src/components/card.js
+++ src/components/card.js
@@ -3,17 +3,23 @@ import PropTypes from "prop-types"
 
 const Card = ({ id, name, type, diet, isExtinct }) => {
   const handleOnClick = () => {
-    console.log("hey")
-    // fetch({
-    //   url: "http://localhost:3022/",
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify({ query: "query allPosts { allPosts { id } }" }),
-    // })
-    //   .then(res => res.json())
-    //   .then(data => console.log({ data }))
+    fetch("http://localhost:3022/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        mutation removeAnimal($id: ID!) {
+          removeAnimal(id: $id)
+        }
+        `,
+        variables: { id },
+      }),
+    })
+      .then(res => res.json())
+      .then(res => console.log(res.data))
   }
-
   return (
     <dl key={id}>
       <dt>{name}</dt>
@@ -32,5 +38,16 @@ Card.propTypes = {
   diet: PropTypes.string.isRequired,
   isExtinct: PropTypes.bool.isRequired,
 }
+// mutation removeAnimal($id: ID!) {
+//   removePost(id: $id)
+// }
+// mutation addPost($id: ID!, $title: String!, $views: Int!, $user_id: ID!) {
+//   createPost(id: $id, title: $title, views: $views, user_id: $user_id) {
+//       id,
+//       title,
+//       views,
+//       user_id
+//   }
+// }
 
 export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Card from "../components/card"
 
 export default function Home({ data }) {
+  const [animals, setAnimals] = useState([])
+  const fetchAnimals = async () => {
+    const apiCall = await fetch("http://localhost:3022/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        query MyQuery {
+          allAnimals {
+            id,
+            name,
+            type,
+            diet,
+            isExtinct,
+          }
+        }
+        `,
+      }),
+    })
+    const results = await apiCall.json()
+
+    setAnimals(results.data.allAnimals)
+  }
+  useEffect(() => {
+    fetchAnimals()
+  }, [animals])
+
   const { jsonGraphQlServer } = data
   return (
     <Layout>
+      {animals.length &&
+        animals.map(animal => {
+          return <Card key={animal.id} {...animal} />
+        })}
       <h1>{data.site.siteMetadata.title}</h1>
       {jsonGraphQlServer.allAnimals.map(animal => {
         return <Card key={animal.id} {...animal} />
