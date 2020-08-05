909d17db4ebf883c30d2b2aabf12e6b2c3e48e08
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-08-05 15:31:26 +0100

    Tidy up unused

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,17 +10,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "JSON_GRAPHQL_SERVER",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "jsonGraphQlServer",
-        // Url to query from
-        url: "http://localhost:3022/",
-      },
-    },
-    {
       resolve: "gatsby-plugin-apollo",
       options: {
         uri: "http://localhost:3022/graphql",
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
-// import React from "react"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import { useQuery, gql } from "@apollo/client"
 import Layout from "../components/layout"
@@ -29,70 +28,17 @@ function Animals() {
 }
 
 export default function Home({ data }) {
-  const [animalData, setAnimalData] = useState({})
-  const fetchAnimals = async () => {
-    const apiCall = await fetch("http://localhost:3022/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: `
-        query MyQuery {
-          allAnimals {
-            id,
-            name,
-            type,
-            diet,
-            isExtinct,
-          }
-        }
-        `,
-      }),
-    })
-    const results = await apiCall.json()
-
-    setAnimalData(results.data)
-    console.log(results.data)
-    console.log(animalData)
-  }
-  useEffect(() => {
-    fetchAnimals()
-  }, [])
-
-  const { jsonGraphQlServer } = data
   return (
     <Layout>
-      <h1>Fetch</h1>
-      {animalData.animals &&
-        animalData.animals.length &&
-        animalData.animals.map(animal => {
-          return <Card key={animal.id} {...animal} />
-        })}
-      <hr />
-      <h1>Apollo</h1>
-      <Animals />
-      <hr />
-      <AddAnimalCard />
       <h1>{data.site.siteMetadata.title}</h1>
-      {jsonGraphQlServer.allAnimals.map(animal => {
-        return <Card key={animal.id} {...animal} />
-      })}
+      <Animals />
+      <AddAnimalCard />
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    jsonGraphQlServer {
-      allAnimals {
-        id
-        name
-        type
-        diet
-        isExtinct
-      }
-    }
     site {
       siteMetadata {
         title
