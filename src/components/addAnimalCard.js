3c373630d94e9200f7223b2e5c13d9bc992e426b
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-08-05 16:11:19 +0100

    Hard coded add animal query

--- src/components/addAnimalCard.js
+++ src/components/addAnimalCard.js
@@ -1,8 +1,45 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { gql, useMutation } from "@apollo/client"
+
+const ADD_ANIMAL = gql`
+  mutation createAnimal(
+    $id: ID!
+    $name: String!
+    $type: String!
+    $diet: String!
+    $isExtinct: Boolean!
+  ) {
+    createAnimal(
+      id: $id
+      name: $name
+      type: $type
+      diet: $diet
+      isExtinct: $isExtinct
+    ) {
+      id
+      type
+      name
+      diet
+      isExtinct
+    }
+  }
+`
 
 const AddAnimalCard = ({}) => {
-  const handleOnClick = () => {}
+  const [addAnimal, { data }] = useMutation(ADD_ANIMAL)
+
+  const handleOnClick = () =>
+    addAnimal({
+      variables: {
+        id: "21",
+        name: "Ligeress",
+        type: "Mammal",
+        diet: "Carnivore",
+        isExtinct: true,
+      },
+    })
+
   return (
     <div>
       <button onClick={handleOnClick}>Add</button>
