46f4ecbe84e0a7ae0c97ce451fdb888ae83fe8fb
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-08-08 13:25:20 +0100

    Aliased test command, removed default data from db

--- server/db.js
+++ server/db.js
@@ -1,8 +1,4 @@
 module.exports = {
-  posts: [
-    { id: 1, title: "Lorem Ipsum", views: 254, user_id: 123 },
-    { id: 2, title: "Sic Dolor amet", views: 65, user_id: 456 },
-  ],
   animals: [
     {
       id: 1,
@@ -38,24 +34,6 @@ module.exports = {
       type: "mammal",
       diet: "herbivore",
       isExtinct: false,
-    },
-  ],
-  users: [
-    { id: 123, name: "John Doe" },
-    { id: 456, name: "Jane Doe" },
-  ],
-  comments: [
-    {
-      id: 987,
-      post_id: 1,
-      body: "Consectetur adipiscing elit",
-      date: new Date("2017-07-03"),
-    },
-    {
-      id: 995,
-      post_id: 1,
-      body: "Nam molestie pellentesque dui",
-      date: new Date("2017-08-17"),
     },
   ],
 }
